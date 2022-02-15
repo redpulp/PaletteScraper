def741665d1b818d012ec2fbe206cd510729264e
Lucio Moriconi <c-lucio@beacon.co.uk>
2022-02-15 14:13:38 +0100

    local test displaying results

---
+++ README.md
@@ -0,0 +1,2 @@
+This project is still a mess, you can enjoy some results by running
+`yarn` and `node src/localTest.js` (Yarn and Node14 required)
---
+++ src/localTest.js
@@ -0,0 +1,16 @@
+const getColors = require('./utils/getColors');
+
+const test = async () => {
+  console.time('getColors');
+  const info = await getColors({
+    headless: true,
+    videoId: 'VPLCk-FTVvw',
+    localFileSystem: true,
+  });
+  console.timeEnd('getColors');
+
+  console.log('Scraped Metadata', info?.videoInfo?.metadata);
+  console.log('Scraped Palettes', info?.videoInfo?.palettes);
+};
+
+test();
